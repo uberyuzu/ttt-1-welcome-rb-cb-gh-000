ea04088abc1cb3e8a6769249d50deb9c002d12bc
Tomoko Himeoka <thimeoka00@mylangara.bc.ca>
2018-10-13 22:30:53 +0000

    automatically backed up by learn

--- lib/welcome.rb
+++ lib/welcome.rb
@@ -1,5 +1,4 @@
 #Edit this file to output "Welcome to Tic Tac Toe!"
 # You can see what this file does by running:
 # ruby lib/welcome.rb from your terminal.
-lib/welcome.#!/usr/bin/env ruby -wKU
-./lib/welcome.rb
+put "Welcome to Tic Tac Toe"
